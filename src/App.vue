cf6febd3bf36493d6fa844c6eb91b6bf1d19f9a0
gexin950228 <861439031@qq.com>
2024-02-29 15:05:05 +0800

    new file:   .eslintrc.cjs 	new file:   .gitignore 	new file:   .vscode/extensions.json 	new file:   README.md 	new file:   index.html 	new file:   jsconfig.json 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.ico 	new file:   src/App.vue 	new file:   src/assets/base.css 	new file:   src/assets/logo.svg 	new file:   src/assets/main.css 	new file:   src/components/HelloWorld.vue 	new file:   src/components/TheWelcome.vue 	new file:   src/components/WelcomeItem.vue 	new file:   src/components/icons/IconCommunity.vue 	new file:   src/components/icons/IconDocumentation.vue 	new file:   src/components/icons/IconEcosystem.vue 	new file:   src/components/icons/IconSupport.vue 	new file:   src/components/icons/IconTooling.vue 	new file:   src/main.js 	new file:   src/router/index.js 	new file:   src/stores/counter.js 	new file:   src/styles/element/index.scss 	new file:   src/views/AboutView.vue 	new file:   src/views/HomeView.vue 	new file:   vite.config.js

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script  setup>
+</script>
+
+<template>
+  <el-button type="primary">Primary</el-button>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+</style>
